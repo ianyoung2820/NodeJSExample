2f0133be781d0bf18300cd445d4952f233d90feb
ianyoung85 <104699600+ianyoung85@users.noreply.github.com>
2024-01-30 19:01:41 -0500

    Added HelloWorld

---
+++ .expo/helloworld.js
@@ -0,0 +1,7 @@
+// helloworld.js
+
+// Define a constant containing your first name
+const myFirstName = "Ian"; // Replace "YourFirstName" with your actual first name
+
+// Print the desired phrase using console.log()
+console.log("Hello", Ian);
